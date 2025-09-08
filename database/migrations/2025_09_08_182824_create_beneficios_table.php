68c1236fa9148e21e321715bf357f7a992623c27
Vcsaransiga <vcsaransiga@utn.edu.ec>
2025-09-08 18:29:01 -0500

    tabla beneficios

---
+++ database/migrations/2025_09_08_182824_create_beneficios_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('beneficios', function (Blueprint $t) {
+            $t->id();
+            $t->string('nombre', 255);
+            $t->text('descripcion')->nullable();
+            $t->string('tipo', 10);           // p.ej: 'FEE','DESC','OTRO'
+            $t->decimal('valor', 7, 2)->nullable();
+            $t->timestampsTz();
+            $t->index(['tipo']);
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('beneficios');
+    }
+};
